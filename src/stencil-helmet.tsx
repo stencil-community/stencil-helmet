8c1c1be0c0b22a95d3133d6a1d892ef5d8691d28
Cam Wiegert <cam@camwiegert.com>
2018-01-31 13:49:57 -0600

    rename renderToHead -> renderNode

--- src/stencil-helmet.tsx
+++ src/stencil-helmet.tsx
@@ -6,13 +6,13 @@ const validTagNames = Object.keys(render);
 const isValidNode = (node: VNode) =>
   validTagNames.indexOf(node.vtag as string) > -1;
 
-const renderToHead = (node: VNode) =>
+const renderNode = (node: VNode) =>
   render[node.vtag](node);
 
 const StencilHelmet = ({ children }: Props) => {
   children
     .filter(isValidNode)
-    .forEach(renderToHead);
+    .forEach(renderNode);
   return <span></span>; // return null once that's supported
 };
 
