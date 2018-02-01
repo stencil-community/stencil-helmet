ccffb62ff67357fb12cf320a805fe9d74b086ee9
Cam Wiegert <cam@camwiegert.com>
2018-02-01 14:30:40 -0600

    feat() update createElement for recursion

--- src/dom.ts
+++ src/dom.ts
@@ -2,13 +2,23 @@ import { VNode } from './types';
 
 const head = document.head;
 
-export const createElement = (tagName, attributes) => {
-  const element = document.createElement(tagName);
-  Object.keys(attributes).forEach(key => {
-    element.setAttribute(key, attributes[key]);
-  });
+export const createElement = ({ vtag, vattrs, vchildren, vtext }: VNode) => {
+  if (vtext !== undefined) {
+    return document.createTextNode(vtext);
+  }
+
+  const element = document.createElement(vtag as string);
+
+  for (const key in vattrs) {
+    element.setAttribute(key, vattrs[key]);
+  }
+
+  for (const child of vchildren) {
+    element.appendChild(createElement(child));
+  }
+
   return element;
 };
 
 export const addElementToHead = (node: VNode) =>
-  head.appendChild(createElement(node.vtag, node.vattrs));
+  head.appendChild(createElement(node));
