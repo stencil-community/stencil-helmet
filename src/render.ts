a3cbb491319adbf1b6cf8576159a9722c5a3177f
Cam Wiegert <cam@camwiegert.com>
2018-02-01 19:22:00 -0600

    feat() add requiredAttrs param to hasAttributes helper

--- src/render.ts
+++ src/render.ts
@@ -4,8 +4,9 @@ import { VNode } from './types';
 const hasChildren = ({ vchildren }: VNode) =>
   Array.isArray(vchildren);
 
-const hasAttributes = ({ vattrs }: VNode) =>
-  typeof vattrs === 'object';
+const hasAttributes = ({ vattrs }: VNode, requiredAttrs: string[] = []) =>
+  typeof vattrs === 'object' &&
+  requiredAttrs.every(vattrs.hasOwnProperty.bind(vattrs));
 
 const isTextNode = ({ vtext }: VNode) =>
   typeof vtext === 'string';
