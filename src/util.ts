7c4882832a91060328ec53792a533589cb3f0bed
Cam Wiegert <cam@camwiegert.com>
2018-02-02 09:05:33 -0600

    fix(util) ensure non-null object in hasAttributes

--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,13 @@
 import { VNode } from './types';
 
+const isObject = (val: any) =>
+  !Array.isArray(val) && val !== null && typeof val === 'object';
+
 export const hasChildren = ({ vchildren }: VNode) =>
   Array.isArray(vchildren);
 
 export const hasAttributes = ({ vattrs }: VNode, requiredAttrs: string[] = []) =>
-  typeof vattrs === 'object' &&
-  requiredAttrs.every(vattrs.hasOwnProperty.bind(vattrs));
+  isObject(vattrs) && requiredAttrs.every(vattrs.hasOwnProperty.bind(vattrs));
 
 export const isTextNode = ({ vtext }: VNode) =>
   typeof vtext === 'string';
