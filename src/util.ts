423b58b2ff194ec8a2dfb63fbb5158d49ab875b8
Cam Wiegert <cam@camwiegert.com>
2018-02-05 14:40:15 -0600

    feat(util) add isElement/isElementArray helpers

--- src/util.ts
+++ src/util.ts
@@ -11,3 +11,9 @@ export const hasAttributes = ({ vattrs }: VNode, requiredAttrs: string[] = []) =
 
 export const isTextNode = ({ vtext }: VNode) =>
   typeof vtext === 'string';
+
+export const isElement = (val: any) =>
+  val instanceof HTMLElement;
+
+export const isElementArray = (val: any) =>
+  Array.isArray(val) && val.every(isElement);
--- test/util.spec.ts
+++ test/util.spec.ts
@@ -1,7 +1,9 @@
 import {
   hasAttributes,
   hasChildren,
-  isTextNode
+  isTextNode,
+  isElement,
+  isElementArray
 } from '../src/util';
 
 describe('hasChildren', () => {
@@ -26,6 +28,24 @@ describe('isTextNode', () => {
     expect(isTextNode({ vtext: '' })).toBe(true);
     expect(isTextNode({ vtext: 'foo' })).toBe(true);
     expect(isTextNode({ vtext: null })).toBe(false);
+  });
+});
+
+describe('isElement', () => {
+  it('checks for instance of HTMLElement', () => {
+    expect(isElement(document.body)).toBe(true);
+    expect(isElement(document.createTextNode('foo')))
+      .toBe(false);
+  });
+});
+
+describe('isElementArray', () => {
+  it('checks for an HTMLElement array', () => {
+    expect(isElementArray([document.body])).toBe(true);
+    expect(isElementArray([
+      document.body,
+      document.createTextNode('foo')
+    ])).toBe(false);
   });
 });
 
